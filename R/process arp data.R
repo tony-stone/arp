c7ca09dc69e1518b656fa39e9e634056a65164a2
tony-stone <tony.stone@sheffield.ac.uk>
2017-02-06 18:14:57 +0000

    re-organised structure

--- R/process arp data.R
+++ R/process arp data.R
@@ -15,37 +15,59 @@ View(summ_data2.1)
 summ_data2.2 <- getARPSummaryData(phase = 2.2)
 View(summ_data2.2)
 
+
+
 # With all plots, below, I recommend viewing them full size on a separate monitor
 
-# Well that was horrible, how does the missingness (by service/time) look graphically?
-examineProblematicData(phase = 1, measuresCombined = TRUE)
-saveProblematicDataImages(phase = 1, measuresCombined = TRUE)
 
+
+# Historic Phase 1 data
 examineProblematicData(phase = 1, measuresCombined = TRUE, startDate = as.Date("2014-10-06"), endDate = as.Date("2016-09-30"))
-examineProblematicData(phase = 2.1, measuresCombined = TRUE)
-missing_and_outlying <- examineProblematicData(phase = 2.2, measuresCombined = TRUE)
+examineProblematicData(phase = 1, measuresCombined = FALSE, startDate = as.Date("2014-10-06"), endDate = as.Date("2016-09-30"))
+examineData(phase = 1, startDate = as.Date("2014-10-06"), endDate = as.Date("2016-09-30"))
+examineCSTriggers(phase = 1, showSDLimits = TRUE, startDate = as.Date("2015-08-31"), endDate = as.Date("2016-09-30"))
 
-# We can do stuff with this is we want
-View(missing_and_outlying)
+saveProblematicDataImages(phase = 1, measuresCombined = TRUE, startDate = as.Date("2014-10-06"), endDate = as.Date("2016-09-30"))
+saveProblematicDataImages(phase = 1, measuresCombined = FALSE, startDate = as.Date("2014-10-06"), endDate = as.Date("2016-09-30"))
 
-# What's missing and what's "outlying"? (by measure/service/time)
+
+
+# Contemporary Phase 1 data
+examineProblematicData(phase = 1, measuresCombined = TRUE, startDate = as.Date("2016-09-30"))
+examineProblematicData(phase = 1, measuresCombined = FALSE, startDate = as.Date("2016-09-30"))
+examineData(phase = 1, startDate = as.Date("2016-09-30"))
+examineCSTriggers(phase = 1, showSDLimits = TRUE, startDate = as.Date("2016-09-30"))
+
+saveProblematicDataImages(phase = 1, measuresCombined = TRUE, startDate = as.Date("2016-09-30"))
+saveProblematicDataImages(phase = 1, measuresCombined = FALSE, startDate = as.Date("2016-09-30"))
+
+
+# All Phase 1 data
+examineProblematicData(phase = 1, measuresCombined = TRUE)
 examineProblematicData(phase = 1, measuresCombined = FALSE)
-examineProblematicData(phase = 1, measuresCombined = FALSE, outliersAtSD = 3, startDate = as.Date("2014-10-06"), endDate = as.Date("2016-09-30"))
-saveProblematicDataImages(phase = 1, measuresCombined = FALSE, outliersAtSD = 3, startDate = as.Date("2014-10-06"), endDate = as.Date("2016-09-30"))
+examineData(phase = 1)
+examineCSTriggers(phase = 1, showSDLimits = TRUE)
 
-examineProblematicData(phase = 2.1, measuresCombined = FALSE, outliersAtSD = 3)
+saveProblematicDataImages(phase = 1, measuresCombined = TRUE)
+saveProblematicDataImages(phase = 1, measuresCombined = FALSE)
 
-examineProblematicData(phase = 2.2, measuresCombined = FALSE, outliersAtSD = 3, endDate = as.Date("2017-01-02"))
-saveProblematicDataImages(phase = 2.2, measuresCombined = FALSE, outliersAtSD = 3, endDate = as.Date("2017-01-02"))
 
-# Now let's look measure by measure
-examineData(phase = 1, outliers_SD = 3, startDate = as.Date("2014-10-06"), endDate = as.Date("2016-09-30"))
-examineData(phase = 2.1, outliers_SD = 3)
-examineData(phase = 2.2, outliers_SD = 3)
+# Phase 2.1 data
+examineProblematicData(phase = 2.1, measuresCombined = TRUE)
+examineProblematicData(phase = 2.1, measuresCombined = FALSE)
+examineData(phase = 2.1)
+examineCSTriggers(phase = 2.1, showSDLimits = TRUE)
 
-# Can (somewhat naively) also just look at the Call Start Triggers
-examineCSTriggers(phase = 1, showSDLimits = TRUE, SDLimits = 3, startDate = as.Date("2015-08-31"), endDate = as.Date("2016-09-30"))
-examineCSTriggers(phase = 2.1, showSDLimits = TRUE, SDLimits = 3)
-examineCSTriggers(phase = 2.2, showSDLimits = TRUE, SDLimits = 3)
 
-# That's enough to be getting on with, surely!
+saveProblematicDataImages(phase = 2.1, measuresCombined = TRUE)
+saveProblematicDataImages(phase = 2.1, measuresCombined = FALSE)
+
+
+# Phase 2.2 data
+examineProblematicData(phase = 2.2, measuresCombined = TRUE)
+examineProblematicData(phase = 2.2, measuresCombined = FALSE, endDate = as.Date("2017-01-02"))
+examineData(phase = 2.2)
+examineCSTriggers(phase = 2.2, showSDLimits = TRUE)
+
+saveProblematicDataImages(phase = 2.2, measuresCombined = TRUE)
+saveProblematicDataImages(phase = 2.2, measuresCombined = FALSE, endDate = as.Date("2017-01-02"))
