9d046a91ba084490d8704a1c6ce2464c73a59e6b
tony-stone <tony.stone@sheffield.ac.uk>
2017-06-01 10:26:29 +0100

    tidying

--- R/main.R
+++ R/main.R
@@ -1,7 +1,4 @@
 # Process raw ARP data
-
-library(data.table)
-
 processARPData(phase = "1")
 processARPData(phase = "2.1")
 processARPData(phase = "2.2")
@@ -10,27 +7,36 @@ processARPData(phase = "_red1")
 processARPData(phase = "_standdown")
 processARPData(phase = "_ruralurban")
 
-# All whole service datacombined data
+
+# Look at stuff
+look.at.what <- "_wholeservice"
+examineProblematicData(phase = look.at.what)
+examineProblematicData(phase = look.at.what, measuresCombined = FALSE)
+saveProblematicDataImages(phase = look.at.what, startDate = as.Date("2016-07-01"))
+saveProblematicDataImages(phase = look.at.what, measuresCombined = FALSE, startDate = as.Date("2016-07-01"))
+examineData(phase = look.at.what, startDate = as.Date("2016-07-01"))
+
+
+
+## Save data for RJ
+# All whole service data combined
 filepath <- combineComparablePhaseData()
 wholeservice.data <- readRDS(filepath)[std_measure_code %in% as.character(c(1:22, 24, 36, 41)) &
-                                      week_beginning >= as.Date("2016-01-04") &
-                                      week_beginning <= as.Date("2016-12-26")]
-all_data_filepath <- saveDataForRJ(wholeservice.data, "std_measure_name", "ARP wholeservice data")
-
-# All comparable data
-red1 <- readRDS(filepath)[week_beginning >= as.Date("2016-01-04") &
+                                         week_beginning >= as.Date("2016-01-04") &
                                          week_beginning <= as.Date("2016-12-26")]
+saveDataForRJ(wholeservice.data, "std_measure_name", "ARP wholeservice data")
 
-examineData(data = red1)
-# examineData(data = wholeservice.data)
-
-
+# Phase 1 data
+fname <- combineWSandPhaseData("1") # combineWSandPhaseData formats the data
+data1 <- readRDS(fname)[phase == "1"]
+examineData(data = data1)
+saveDataForRJ(data1, "measure_name", "ARP Phase 1 data")
 
 # Phase 2.1 data
-fname <- combineWSandPhaseData("2.1")
+fname <- combineWSandPhaseData("2.2")
 data2.1 <- readRDS(fname)[phase == "2.1"]
 examineData(data = data2.1)
-saveDataForRJ(data2.2, "measure_name", "ARP Phase 2.2 data")
+saveDataForRJ(data2.1, "measure_name", "ARP Phase 2.1 data")
 
 # Phase 2.2 data
 fname <- combineWSandPhaseData("2.2")
@@ -52,8 +58,6 @@ data_standdown <- readRDS(fname)
 saveDataForRJ(data_standdown, "std_measure_name", "ARP stand-down data")
 
 
-
-
 # Combine Phase 1, 2.1, 2.2 data from YAS, SWAS, WMAS with urban rural data
 fname <- combineUrbanRuralWithStdMeasures()
 data_urbanrural <- readRDS(fname)
@@ -61,8 +65,7 @@ data_urbanrural <- readRDS(fname)
 saveDataForRJ(data_urbanrural, "std_measure_name", "ARP urban-rural data")
 
 
-
-# lookup for Richard
+## lookup for Richard
 bla <- unique(data_urbanrural[, .(std_measure_name, measure_code, measure, sub_measure, measure_type, call_level)])
 setorder(bla, std_measure_name)
 write.csv(bla[, .(std_measure_name, measure_code, measure, sub_measure, measure_type, call_level)], file = "output_data/rural urban data measure lookup.csv", row.names = FALSE)
